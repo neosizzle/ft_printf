f95c2526cfacdf12ee90a43ec485d8d2f65d2709
neosizzle <junhanngd1115@gmail.com>
2021-08-06 01:18:36 +0100

    percent

--- srcs/ft_handlepercent.c
+++ srcs/ft_handlepercent.c
@@ -1,3 +1,8 @@
 #include "ft_printf.h"
 
 //this file has functions that handles the percent sign
+void	ft_handlepercent(int *print_len, t_format format, va_list argp)
+{
+	ft_putchar_fd('%' , 1);
+	(*printlen++);
+}
