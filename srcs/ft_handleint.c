6fa7b7b669a3e25ad57512160ceb73f7d8d860f5
neosizzle <junhanngd1115@gmail.com>
2021-08-06 03:08:04 +0100

    m

--- srcs/ft_handleint.c
+++ srcs/ft_handleint.c
@@ -58,12 +58,21 @@ void	ft_handleint(int *print_len, t_format format, va_list argp)
 
 	num = va_arg(argp, int);
 	num_len = get_signed_num_len(num);
-	if (format.width.exist)
-		ft_handle_width(print_len, format.width.value - num_len, 1);
+	num_str = ft_itoa(num);
+	if (format.is_minus)
+	{
+		write(1, num_str, num_len);
+		if (format.width.exist)
+			ft_handle_width(print_len, format.width.value - num_len, 1);
+	}
+	else
+	{
+		if (format.width.exist)
+			ft_handle_width(print_len, format.width.value - num_len, 1);
+		write(1, num_str, num_len);	
+	}
 	if (format.percision.exist)
 		ft_handle_percision(print_len, num, format.percision.value);
-	num_str = ft_itoa(num);
-	write(1, num_str, num_len);
 	(*print_len) += num_len;
 	free(num_str);
 }
