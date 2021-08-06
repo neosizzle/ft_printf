17de377c8c68fa5f4b7e202c478f88d1e9ec37f9
neosizzle <junhanngd1115@gmail.com>
2021-08-06 04:01:10 +0100

    -

--- srcs/ft_handleint.c
+++ srcs/ft_handleint.c
@@ -58,10 +58,19 @@ void	ft_handleint(int *print_len, t_format format, va_list argp)
 
 	num = va_arg(argp, int);
 	num_len = get_signed_num_len(num);
-	if (format.width.exist)
-		ft_handle_width(print_len, format.width.value - num_len, 1);
 	num_str = ft_itoa(num);
-	write(1, num_str, num_len);
+	if (format.is_minus)
+	{
+		write(1, num_str, num_len);
+		if (format.width.exist)
+			ft_handle_width(print_len, format.width.value - num_len, 1);
+	}
+	else
+	{
+		if (format.width.exist)
+			ft_handle_width(print_len, format.width.value - num_len, 1);
+		write(1, num_str, num_len);
+	}
 	(*print_len) += num_len;
 	free(num_str);
 }
