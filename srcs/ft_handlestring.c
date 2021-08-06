12fb065af705efd877294f64749460b90c4ca85b
neosizzle <junhanngd1115@gmail.com>
2021-08-06 08:43:27 +0100

    asdf

--- srcs/ft_handlestring.c
+++ srcs/ft_handlestring.c
@@ -2,6 +2,20 @@
 
 //this file has functions that handles the strings
 
+static void	width_write(t_format format, char *string, int len, int *p_l)
+{
+	if (format.width.exist)
+			ft_handle_width(p_l, format.width.value - (int)len, 1);
+	write(1, string, len);
+}
+
+static void	write_width(t_format format, char *string, int len, int *p_l)
+{
+	write(1, string, len);
+	if (format.width.exist)
+			ft_handle_width(p_l, format.width.value - (int)len, 1);
+}
+
 //handle str (main func)
 //ft_putstr_fd("(null)", 1);
 void	ft_handlestr(int *print_len, t_format format, va_list argp)
@@ -20,16 +34,8 @@ void	ft_handlestr(int *print_len, t_format format, va_list argp)
 	if (format.percision.exist)
 		len = ft_min(ft_strlen(string), format.percision.value);
 	if (format.is_minus)
-	{
-		write(1, string, len);
-		if (format.width.exist)
-			ft_handle_width(print_len, format.width.value - (int)len, 1);
-	}
+		write_width(format, string, len, p_l);
 	else
-	{
-		if (format.width.exist)
-			ft_handle_width(print_len, format.width.value - (int)len, 1);
-		write(1, string, len);
-	}
+		width_write(format, string, len, p_l);
 	*print_len += (int)len;
 }
