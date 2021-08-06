94d1a1943ebaa9642bf74eb4654a91ef58c617cb
neosizzle <junhanngd1115@gmail.com>
2021-08-06 02:15:18 +0100

    asdf

--- srcs/ft_handlestring.c
+++ srcs/ft_handlestring.c
@@ -14,8 +14,16 @@ void	ft_handlestr(int *print_len, t_format format, va_list argp)
 	len = ft_strlen(string);
 	if (format.percision.exist)
 		len = ft_min(ft_strlen(string), format.percision.value);
-	if (format.width.exist)
-		ft_handle_width(print_len, format.width.value - (int)len, 1);
-	write(1, string, len);
+	if (format.is_minus)
+	{
+		write(1, string, len);
+		if (format.width.exist)
+			ft_handle_width(print_len, format.width.value - (int)len, 1);
+	}
+	else{
+		if (format.width.exist)
+			ft_handle_width(print_len, format.width.value - (int)len, 1);
+		write(1, string, len);
+	}
 	*print_len += (int)len;
 }
