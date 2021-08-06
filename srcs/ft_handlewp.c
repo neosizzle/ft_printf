ff56184ce59a828f36ff92f5245a30870ca47821
neosizzle <junhanngd1115@gmail.com>
2021-08-06 04:12:09 +0100

    0

--- srcs/ft_handlewp.c
+++ srcs/ft_handlewp.c
@@ -17,3 +17,27 @@ void	ft_handle_width(int *print_len, int width, int is_space)
 	}
 	
 }
+
+void	ft_handle_width_num(int *print_len, int width, int is_space, char **num_str, int num)
+{
+	char	c;
+
+	if (is_space)
+		c = ' ';
+	else
+		c = '0';
+	if (num < 0 && !is_space)
+	{
+		ft_putchar_fd('-',1);
+		(*num_str)++;
+		(*print_len)++;
+		width--;
+	}
+	while (width > 0)
+	{
+		ft_putchar_fd(c,1);
+		width--;
+		(*print_len)++;
+	}
+	
+}
